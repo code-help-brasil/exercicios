5b1670c120ae1ff48704d2389500511c57dd5ade
Nicolas Pantoja <nicolaspantoja@hotmail.com>
2023-04-30 11:09:01 -0400

    add: Ex002

---
+++ resolucoes/php/lista01/ex002.php
@@ -0,0 +1,7 @@
+<?php
+/* 2. Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas para ela. ex:
+* Exemplo: Olá Carlos tudo bem;
+*/
+
+$name = readline("Digite Seu nome: ");
+echo "\rOlá ".$name." Bem vindo(a) ao mundo DeV";
