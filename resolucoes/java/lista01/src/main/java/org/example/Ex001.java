26df837ba0d2a31eaa82383de12faaea7693c771
Indra <42115827+Linozduart@users.noreply.github.com>
2023-04-30 13:04:10 -0300

    Adicionando Ex001

---
+++ resolucoes/java/lista01/src/main/java/org/example/Ex001.java
@@ -0,0 +1,12 @@
+package org.example;
+
+
+public class Ex001 {
+    public void run(){
+        System.out.println("Hello world!");
+    }
+
+    public static void main(String[] args) {
+        new Ex001().run();
+    }
+}
