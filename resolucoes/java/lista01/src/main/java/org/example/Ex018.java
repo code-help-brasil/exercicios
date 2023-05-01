e18b701efb5b8d81f8a573198b009f2c72eab29b
Indra <42115827+Linozduart@users.noreply.github.com>
2023-05-01 02:35:19 -0300

    Update

---
+++ resolucoes/java/lista01/src/main/java/org/example/Ex018.java
@@ -0,0 +1,34 @@
+/*Faça um programa que leia o ano de nascimento de uma pessoa, 
+calcule a idade dela e depois mostre se ela pode ou não votar. */
+
+package org.example;
+
+import java.util.Calendar;
+import java.util.Scanner;
+import java.time.Year;
+
+public class Ex018 {
+    public static void main(String[] args) {
+        Ex018.main();
+    }
+
+    public static void main() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Qual ano você nasceu ?: ");
+        int anoNasc = sc.nextInt();
+        int anoAtual = Year.now().getValue();
+        int idade = anoAtual - anoNasc;
+
+        if (idade < 16) {
+            System.out.println("Você é menor de idade e não pode votar");
+        } else if (idade >= 18 && idade < 65) {
+            System.out.println("Voto para você é obrigatório");
+        } else {
+            System.out.println("O voto para você é opcional");
+        }
+
+        sc.close();
+
+    }
+}
