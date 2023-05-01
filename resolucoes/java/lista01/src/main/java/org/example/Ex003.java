248518897f2b9f28cd4a81cd772167e84b1382e2
Indra <42115827+Linozduart@users.noreply.github.com>
2023-04-30 22:07:10 -0300

    Update

--- resolucoes/java/lista01/src/main/java/org/example/Ex003.java
+++ resolucoes/java/lista01/src/main/java/org/example/Ex003.java
@@ -14,20 +14,19 @@ public class Ex003 {
 
     public static void main(String[] args) {
         Ex003.main();
-        
+
     }
+
     public static void main() {
         Scanner sc = new Scanner(System.in);
         System.out.print("Qual nome do funcionario?: ");
         String nome = sc.nextLine();
-        System.out.printf("Qual o salário de(a) %s ?: ",nome);
+        System.out.printf("Qual o salário de(a) %s ?: ", nome);
         Float salario = Float.parseFloat(sc.nextLine());
-        System.out.printf("Nome do Funcionário: %s ",nome);
-        System.out.printf("Salário: R$%s %n",salario);
-        System.out.printf("funcionário %s tem um salário de R$%s em Junho.%n",nome,salario);
+        System.out.printf("Nome do Funcionário: %s ", nome);
+        System.out.printf("Salário: R$%s %n", salario);
+        System.out.printf("funcionário %s tem um salário de R$%s em Junho.%n", nome, salario);
         sc.close();
     }
 
-
-   
 }
