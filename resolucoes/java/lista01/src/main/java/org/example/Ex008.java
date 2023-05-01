97aa36feb4ccd33ed0baa71db4abf8fbe402e5a3
Indra <42115827+Linozduart@users.noreply.github.com>
2023-04-30 23:28:24 -0300

    Update

---
+++ resolucoes/java/lista01/src/main/java/org/example/Ex008.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Ex008 {
+    public static void main(String[] args) {
+        Ex008.main();
+    }
+    public static void main() {
+        Scanner sc = new Scanner()
+        
+    }
+}
