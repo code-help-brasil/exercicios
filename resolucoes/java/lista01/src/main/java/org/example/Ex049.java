a024ff317c369d76677e22585031444fad694e01
Indra <42115827+Linozduart@users.noreply.github.com>
2023-05-03 18:37:24 -0300

    update

--- resolucoes/java/lista01/src/main/java/org/example/Ex049.java
+++ resolucoes/java/lista01/src/main/java/org/example/Ex049.java
@@ -2,7 +2,6 @@
 Crie um programa que leia 6 números inteiros e no final mostre quantos deles são pares e quantos são ímpares.
  */
 
-
 package org.example;
 
 import java.util.Scanner;
@@ -15,18 +14,22 @@ public class Ex049 {
     public static void main() {
         Scanner sc = new Scanner(System.in);
         int i = 1;
-        int soma = 0;
+        int numero = 0;
+        int par = 0;
+        int impar = 0;
         while (i <= 6) {
             System.out.print("Digite um número: ");
-            soma += sc.nextInt();
+            numero = sc.nextInt();
             i++;
+            if (numero % 2 == 0) {
+                par += 1;
+            } else {
+                impar += 1;
+            }
         }
-
-
-        
-        System.out.printf("Soma entre os numeros foi %d", soma);
+        System.out.printf("Foram digitados %d numeros pares e %d impares", par, impar);
         sc.close();
 
     }
-    
+
 }
