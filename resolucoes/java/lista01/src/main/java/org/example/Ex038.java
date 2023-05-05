476588b3e149304c6f04734c20f837e908f2e7f2
Indra <42115827+Linozduart@users.noreply.github.com>
2023-05-04 22:53:36 -0300

    Update

--- resolucoes/java/lista01/src/main/java/org/example/Ex038.java
+++ resolucoes/java/lista01/src/main/java/org/example/Ex038.java
@@ -2,17 +2,18 @@
 Escreva um programa que mostre na tela a seguinte contagem:
     6 7 8 9 10 11 Acabou!
 */
-
+package org.example;
 public class Ex038
 {  
-    public void run038(){
+    public static void main(String[] args){
+        Ex038.main();
+    }
+    public static void main(){
         for(int i = 6; i <= 11; i++){
             System.out.print(i + " ");
         }
         System.out.println("Acabou!");
     }
     
-    public static void main(String[] args){
-        new Exercicios().run038();
-    }
+    
 }
