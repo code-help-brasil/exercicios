e925a63b9e9f501605faf2efe6af5552ed5c2f9f
Indra <42115827+Linozduart@users.noreply.github.com>
2023-05-14 17:54:13 -0300

    update

--- resolucoes/java/lista01/src/main/java/org/example/Ex096.java
+++ resolucoes/java/lista01/src/main/java/org/example/Ex096.java
@@ -6,6 +6,25 @@
 
 package org.example;
 
+import java.util.Scanner;
+
 public class Ex096 {
-    
+
+    public static Float Media(Float nota1,Float nota2) {
+        return nota1/nota2;
+    }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite a primeira nota: ");
+        Float nota1 = sc.nextFloat();
+
+        System.out.print("Digite a segunda nota: ");
+        Float nota2 = sc.nextFloat();
+
+        System.out.print(Media(nota1,nota2));
+        
+        sc.close();
+    }
 }
